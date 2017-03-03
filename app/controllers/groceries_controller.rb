23be0d33a72262c7b0916ef7a273c01b6f33b9d2
Wen Zhang <iwenyou@gmail.com>
2017-03-02 17:53:56 -0800

    add groceries controller

---
+++ app/controllers/groceries_controller.rb
@@ -0,0 +1,16 @@
+class GroceriesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'groceries/index'
+
+  get 'groceries/new'
+
+  get 'groceries/show'
+
+  get 'groceries/edit'
+
+  get 'groceries/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
+  recources :groceries
 
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
