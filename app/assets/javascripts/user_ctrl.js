ab590543fc5d52ce5fbb22c3a35f3d71b1b1743a
Wen Zhang <iwenyou@gmail.com>
2017-03-20 17:30:03 -0700

    daily update - working on user internal api

--- app/assets/javascripts/user_ctrl.js
+++ app/assets/javascripts/user_ctrl.js
@@ -1,9 +1,17 @@
-// (function() {
-//     "use strict";
-//
-//     angular.module("app").controller("groceryCtrl", function($scope, $http) {
-//
-//
-//
-//     });
-// }());
+(function() {
+    "use strict";
+
+    angular.module("app").controller("userCtrl", function($scope, $http) {
+
+      $scope.message = "hello world";
+
+      $scope.userSetup = function(){
+        $http.get("/api/v1/users.json").then(function(response){
+          console.log(response.data);
+          $scope.user = response.data;
+        });
+      };
+
+
+    });
+}());
